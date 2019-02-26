cbecf7dce8bc55c6f70a9a9b80f5fd1a32f2c025
bobbypine <45605113+bobbypine@users.noreply.github.com>
2019-02-26 17:06:17 -0500

    Update 2/26/19
    No longer scrapes the 'new' category but rather loops through all categories to find the keyword. Should the 'new' category come back starting with week two of SS'19 the script will not search it to avoid duplicate results.

--- Keyword.py
+++ Keyword.py
@@ -12,21 +12,25 @@ def keysearch(key):
     mylist = []
     global mylists
     mylists = mylist
-    for result in data['products_and_categories']['new']:
-        if keyword in result['name'].lower():
-            print('Product Found!')
-            name = result['name']
-            id = result['id']
-            cat = result['category_name']
-            price = '${}'.format(result['price']*.01)
-            link = 'https://www.supremenewyork.com/shop/{}'.format(id)
-            mylist.append(id)
-            print(len(mylist), end=""),
-            print('.)', end = ""),
-            print(name,'-',cat, '-', price)
-            webbrowser.open(link)
-            print('Product Found and Opened in {:.2f} Seconds'.format(time.time()-starttime))
-            print()
+    for items in data['products_and_categories']:
+        if items != 'new':
+            categories = items
+        for x in categories.split():
+            for result in data['products_and_categories']['{}'.format(x)]:
+                if keyword in result['name'].lower():
+                    print('Product Found!')
+                    name = result['name']
+                    id = result['id']
+                    cat = result['category_name']
+                    price = '${}'.format(result['price']*.01)
+                    link = 'https://www.supremenewyork.com/shop/{}'.format(id)
+                    mylist.append(id)
+                    print(len(mylist), end=""),
+                    print('.)', end = ""),
+                    print(name,'-',cat, '-', price)
+                    webbrowser.open(link)
+                    print('Product Found and Opened in {:.2f} Seconds'.format(time.time()-starttime))
+                    print()
 
 keyword = input('Enter Keyword, Hit Enter When Ready:').lower()
 print()
